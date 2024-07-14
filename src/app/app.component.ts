a867e961ecaf31aafb526b9a139c551c5402fbc5
Luis Gines <legines@gmail.com>
2024-07-14 15:38:57 -0400

    Using videogular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, VgCoreModule, VgControlsModule, VgOverlayPlayModule, VgBufferingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
